bb13ef0c6867e05a6598ab5c2e8a0e88d037a672
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-06 17:23:40 -0300

    v1.2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,7 +8,8 @@ fun main() {
     val giovanni = Funcionario(
         nome = "Giovanni",
         cpf = "111.111.111-11",
-        salario = 1000.0
+        salario = 1000.0,
+        tipo = 0
     )
 
     println("nome ${giovanni.nome}")
