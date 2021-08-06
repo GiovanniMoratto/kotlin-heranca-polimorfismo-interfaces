fe49a5f3420ca46fd7e4f4e6555f3d9dfa7b6857
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-06 17:29:09 -0300

    v1.3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,11 +9,29 @@ fun main() {
         nome = "Giovanni",
         cpf = "111.111.111-11",
         salario = 1000.0,
-        tipo = 0
     )
 
     println("nome ${giovanni.nome}")
     println("cpf ${giovanni.cpf}")
     println("salario ${giovanni.salario}")
     println("bonificação ${giovanni.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
+
+    if (fran.autentica(1234)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autentição")
+    }
+
 }
