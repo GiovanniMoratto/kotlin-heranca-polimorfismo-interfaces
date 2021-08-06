38a0945c8b5232728a4a8fcfc481c497050ee162
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-06 18:39:24 -0300

    v2.3

---
+++ src/main/kotlin/SistemaInterno.kt
@@ -0,0 +1,14 @@
+/**
+ *@Author giovanni.moratto
+ */
+
+class SistemaInterno {
+
+    fun entra(admin: FuncionarioAdmin, senha: Int) {
+        if (admin.autentica(senha)) {
+            println("Bem vindo ao Bytebank")
+        } else {
+            println("Falha na autenticação")
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,25 @@
  */
 
 fun main() {
-    println("Bem vindo ao Bytebank")
+    testaAutenticacao()
+}
 
-    testaComportamentosConta()
+fun testaAutenticacao() {
+    val gerente = Gerente(
+        nome = "Giovanni",
+        cpf = "111.111.111-11",
+        salario = 1000.0,
+        senha = 1000
+    )
+    val diretora = Diretor(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 2000,
+        plr = 200.0
+    )
+
+    val sistema = SistemaInterno()
+    sistema.entra(gerente, 2000)
+    sistema.entra(diretora, 1000)
 }
